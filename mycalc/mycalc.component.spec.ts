4a019b2723eecd7a56e4d7fa8ed8eddd8dfae382
nazrinayesha <168328634+nazrinayesha@users.noreply.github.com>
2024-07-21 12:37:49 +0530

    Add files via upload

---
+++ mycalc/mycalc.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MycalcComponent } from './mycalc.component';
+
+describe('MycalcComponent', () => {
+  let component: MycalcComponent;
+  let fixture: ComponentFixture<MycalcComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MycalcComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MycalcComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
